c18f8a3bd0aff71853446678d936cc7465fefa3a
Jared Tanner <Deraj21@gmail.com>
2020-06-20 15:54:02 -0700

    added file scructure; added test scene

---
+++ src/scenes/Game.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+  constructor(){
+    super('game')
+
+    this.player = undefined
+  }
+
+  preload(){
+    this.load.image('sky', 'assets/bg_layer.png')
+    this.load.image('bunny1', 'assets/bunny_stand.png')
+  }
+
+  create(){
+    const center = {
+      x: this.scale.width / 2,
+      y: this.scale.height / 2
+    }
+
+    this.player = this.physics.add.sprite(center.x, center.y, 'bunny1')
+  }
+
+  update(){
+
+  }
+}
